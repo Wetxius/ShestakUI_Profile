8a2b52e0f4a6a806d0e85ebe78c0b236fba611ce
Wetxius <Wetxius@users.noreply.github.com>
2020-09-17 19:15:20 +0300

    Update Profiles.lua

--- ShestakUI_Profile/Profiles.lua
+++ ShestakUI_Profile/Profiles.lua
@@ -9,27 +9,18 @@ ShestakUICustomProfile = function()
 
 	-- Change UI settings (ShestakUI/Config/Settings.lua)
 	C["general"].welcome_message = false
-	C["misc"].shift_marking = false
 	C["general"].vehicle_mouseover = true
-	C["misc"].quest_auto_button = true
-	C["trade"].disenchanting = true
-	C["misc"].click_cast = true
-	C["trade"].enchantment_scroll = true
-	C["announcements"].bad_gear = true
-	C["automation"].accept_invite = true
-	C["automation"].screenshot = true
-	C["automation"].auto_role = true
-	C["automation"].tab_binder = true
 	C["skins"].blizzard_frames = true
 	C["skins"].minimap_buttons = true
 	C["skins"].bigwigs = true
-	C["combattext"].crit_prefix = ""
-	C["combattext"].blizz_head_numbers = true
-	C["combattext"].dk_runes = false
-	C["combattext"].heal_treshold = UnitHealthMax("player")/100
-	C["pulsecooldown"].enable = true
-	C["pulsecooldown"].threshold = 6
-	C["threat"].enable = false
+	C["unitframe"].castbar_icon = true
+	C["unitframe"].plugins_enemy_spec = true
+	C["raidframe"].solo_mode = true
+	C["raidframe"].icons_leader = false
+	C["aura"].show_spiral = true
+	C["aura"].cast_by = true
+	C["actionbar"].toggle_mode = false
+	C["actionbar"].bottombars = 3
 	C["tooltip"].hide_combat = true
 	C["tooltip"].rank = false
 	C["tooltip"].spell_id = true
@@ -40,25 +31,34 @@ ShestakUICustomProfile = function()
 	C["chat"].chat_bar = true
 	C["chat"].chat_bar_mouseover = true
 	C["chat"].combatlog = false
-	C["minimap"].toggle_menu = false
-	C["minimap"].fog_of_war = true
 	C["nameplate"].health_value = true
 	C["nameplate"].healer_icon = true
-	C["actionbar"].toggle_mode = false
-	C["actionbar"].bottombars = 3
-	C["aura"].show_spiral = true
-	C["aura"].cast_by = true
+	C["combattext"].crit_prefix = ""
+	C["combattext"].blizz_head_numbers = true
+	C["combattext"].dk_runes = false
+	C["combattext"].heal_treshold = UnitHealthMax("player")/100
+	C["minimap"].toggle_menu = false
+	C["minimap"].fog_of_war = true
 	C["filger"].show_tooltip = true
-	C["unitframe"].castbar_icon = true
-	C["unitframe"].plugins_enemy_spec = true
-	C["raidframe"].solo_mode = true
-	C["raidframe"].icons_leader = false
+	C["announcements"].bad_gear = true
+	C["automation"].accept_invite = true
+	C["automation"].screenshot = true
+	C["automation"].auto_role = true
+	C["automation"].tab_binder = true
+	C["pulsecooldown"].enable = true
+	C["pulsecooldown"].threshold = 6
+	C["threat"].enable = false
 	C["toppanel"].enable = false
 	C["stats"].battleground = true
 	C["stats"].currency_cooking = false
 	C["stats"].currency_professions = false
 	C["stats"].currency_raid = false
 	C["stats"].currency_misc = false
+	C["trade"].disenchanting = true
+	C["trade"].enchantment_scroll = true
+	C["misc"].shift_marking = false
+	C["misc"].quest_auto_button = true
+	C["misc"].click_cast = true
 	
 	-- Change UI fonts (ShestakUI/Config/Fonts.lua)
 	C["font"].stats_font = C.media.normal_font
@@ -84,5 +84,17 @@ ShestakUICustomProfile = function()
 		GameTooltipHeader:SetFont(C.media.normal_font, 14)
 		Tooltip_Small:SetFont(C.media.normal_font, 10)
 		ChatBubbleFont:SetFont(C.media.normal_font, 15)
+	end)
+	
+	-- Change raid layout depend of class
+	local frame = CreateFrame("Frame")
+	frame:RegisterEvent("VARIABLES_LOADED")
+	frame:SetScript("OnEvent", function()
+		ShestakUISettings = _ShestakUISettings or {}
+		if T.class == "PALADIN" then
+			ShestakUISettings.RaidLayout = "HEAL"
+		else
+			ShestakUISettings.RaidLayout = "DPS"
+		end
 	end)
 end
